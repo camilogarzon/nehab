e20619bf5ce03e712e816edb527d0d06f1bac995
allejo392 <alejo_chiqui@hotmail.com>
2013-10-19 10:43:58 -0500

    intento con main

--- Codigo/TestProcessing/src/MyProcessingSketch.java
+++ Codigo/TestProcessing/src/MyProcessingSketch.java
@@ -4,60 +4,67 @@ import SimpleOpenNI.*;
 public class MyProcessingSketch extends PApplet {
 	SimpleOpenNI context;
 	PVector jointPos = new PVector();
-	PVector com = new PVector();                                   
-	PVector com2d = new PVector(); 
+	PVector com = new PVector();
+	PVector com2d = new PVector();
+	
+public MyProcessingSketch(){ 
+	this.setup();
+	this.draw();
+}	
 
-  public void setup() {
-	  size(640,480);
-	  
-	  context = new SimpleOpenNI(this);
-	  // enable depthMap generation 
-	  context.enableDepth();
-	   
-	  // enable skeleton generation for all joints
-	  context.enableUser();
-  }
+	public void setup() {
+		
+		size(640, 480);
 
-  public void draw() {
-	  context.startTrackingSkeleton(1);
-	  PImage depth = context.depthImage();
-	image(depth, 0, 0);
-	  IntVector blabla= new IntVector();
-	  
-	  context.update();
-	  context.getUsers(blabla); // <----------------!!!!!!!!!!!!!
-	 // int userId = blabla.get(0);
-	   if(context.getCoM(1,com))
-	    {
-	      context.convertRealWorldToProjective(com,com2d);
-	      stroke(100,255,0);
-	      strokeWeight(1);
-	      beginShape(LINES);
-	        vertex(com2d.x,com2d.y - 5);
-	        vertex(com2d.x,com2d.y + 5);
+		context = new SimpleOpenNI(this);
+		// enable depthMap generation
+		context.enableDepth();
 
-	        vertex(com2d.x - 5,com2d.y);
-	        vertex(com2d.x + 5,com2d.y);
-	      endShape();
-	      
-	      fill(0,255,100);
-	    //  text(Integer.toString(userId),com2d.x,com2d.y);
-	    }
-	  if (blabla.size() > 0) {
-	// get the first user
-	int userId2 = blabla.get(0);
+		// enable skeleton generation for all joints
+		context.enableUser();
 
-	  context.getJointPositionSkeleton(userId2,SimpleOpenNI.SKEL_RIGHT_HAND,jointPos);
-	  PVector convertedRightHand = new PVector();
-	 // println(jointPos);
-
-	  context.convertRealWorldToProjective(jointPos,convertedRightHand);
-	 // image(context.userImage(),0,0);
-	  //println("x= "+ convertedRightHand.x);
-	  //println("y= "+ convertedRightHand.y);
-	  fill(255,0,0);
-	  ellipse(convertedRightHand.x, convertedRightHand.y, 25, 25); 
-	 // context.drawLimb(userId2, SimpleOpenNI.SKEL_LEFT_ELBOW, SimpleOpenNI.SKEL_LEFT_HAND);
 	}
-  }
+
+	public void draw() {
+		
+		 
+		context.startTrackingSkeleton(1);
+		PImage depth = context.depthImage();
+		image(depth, 0, 0);
+		IntVector blabla = new IntVector();
+		context.update();
+		context.getUsers(blabla); // <----------------!!!!!!!!!!!!!
+		// int userId = blabla.get(0);
+		if (context.getCoM(1, com)) {
+			context.convertRealWorldToProjective(com, com2d);
+			stroke(100, 255, 0);
+			strokeWeight(1);
+			beginShape(LINES);
+			vertex(com2d.x, com2d.y - 5);
+			vertex(com2d.x, com2d.y + 5);
+
+			vertex(com2d.x - 5, com2d.y);
+			vertex(com2d.x + 5, com2d.y);
+			endShape();
+
+			fill(0, 255, 100);
+			// text(Integer.toString(userId),com2d.x,com2d.y);
+		}
+		if (blabla.size() > 0) {
+			// get the first user
+			int userId2 = blabla.get(0);
+
+			context.getJointPositionSkeleton(userId2,
+					SimpleOpenNI.SKEL_RIGHT_HAND, jointPos);
+			PVector convertedRightHand = new PVector();
+			// println(jointPos);
+
+			context.convertRealWorldToProjective(jointPos, convertedRightHand);
+			fill(255, 0, 0);
+			ellipse(convertedRightHand.x, convertedRightHand.y, 25, 25);
+
+		}
+		
+	}
 }
+		
